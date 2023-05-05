5f6ea3220aa03db3d4b56e5eb6f6d77f6aa717b9
gabref <gabrielfranzer@gmail.com>
2023-05-05 09:49:59 -0300

    feat: lsp basics

---
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,48 @@
+local status, neodev = pcall(require, 'neodev')
+if (not status) then return end
+
+-- lua and plugin docs, help and completion
+neodev.setup()
+
+-- lsp zero
+local statusl, lsp = pcall(require, 'lsp-zero')
+if (not statusl) then return end
+
+lsp.preset({})
+
+lsp.ensure_installed({
+    'tsserver',
+    'rust_analyzer'
+})
+
+-- fix undefined global vim
+lsp.configure('lua-language-server', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set({'n', 'x'}, '<leader>f', function() vim.lsp.buf.format() end, opts)
+end)
+
+
+
+lsp.setup()
