d74054f9ece1c6200d6861eac33c04e4c64dad4d
gabref <gabrielfranzer@gmail.com>
2024-06-03 02:40:29 +0200

    fix: call tree sitter with configured options

--- nvim/.config/nvim/lua/gabre/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/gabre/plugins/treesitter.lua
@@ -60,7 +60,8 @@ return {
 				},
 			}
 		end,
-		config = function()
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
 			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 			parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
 		end,
