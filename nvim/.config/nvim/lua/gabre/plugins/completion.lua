04f4e686b96fcf4a869b55de0b52f74275156ec7
gabref <gabrielfranzer@gmail.com>
2024-06-03 02:26:17 +0200

    feat: some stuff added to completion

--- nvim/.config/nvim/lua/gabre/plugins/completion.lua
+++ nvim/.config/nvim/lua/gabre/plugins/completion.lua
@@ -1,18 +1,27 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
+		event = { "InsertEnter", "CmdlineEnter" },
 		dependencies = {
-			"onsails/lspkind.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-buffer",
-			"onsails/lspkind.nvim",
 			{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"onsails/lspkind.nvim",
+			"onsails/lspkind.nvim",
 			"saadparwaiz1/cmp_luasnip",
 		},
 		config = function()
-			require "gabre.completion"
+			require("gabre.completion")
 		end,
 	},
 }
