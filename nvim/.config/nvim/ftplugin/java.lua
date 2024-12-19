de62e7dbee912a45c40cecf68e6fcb4d519851a9
gabref <gabrielfranzer@gmail.com>
2024-12-19 11:41:09 +0100

    feat: fresh start

--- nvim/.config/nvim/ftplugin/java.lua
+++
@@ -1,6 +0,0 @@
-local config = {
-	cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
-	root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', 'java_root', '.git', 'mvnw' }, { upward = true })[1]),
-}
-
-require('jdtls').start_or_attach(config)
