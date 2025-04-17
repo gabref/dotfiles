43832b2996ede6f26ac6034ddfaffc93a08b3b12
gabref <gabrielfranzer@gmail.com>
2025-04-17 11:04:25 +0200

    bug: blog comments done write with my mapping

--- nvim/.config/nvim/lua/gabre/plugins/comment.lua
+++ nvim/.config/nvim/lua/gabre/plugins/comment.lua
@@ -13,14 +13,17 @@ return {
 		dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
 		-- stylua: ignore
 		keys = {
-			{ '<leader>;', '<Plug>(comment_toggle_blockwise_current)', mode = 'n', desc = 'Comment' },
-			{ '<leader>;', '<Plug>(comment_toggle_blockwise_visual)',  mode = 'x', desc = 'Comment' },
+			{ '<leader>;', '<Plug>(comment_toggle_linewise_current)', mode = 'n', desc = 'Comment' },
+			{ '<leader>;', 'V<Plug>(comment_toggle_blockwise_visual)',  mode = 'x', desc = 'Comment' },
 		},
 		opts = function(_, opts)
 			local ok, tcc =
 				pcall(require, 'ts_context_commentstring.integrations.comment_nvim')
 			if ok then
 				opts.pre_hook = tcc.create_pre_hook()
+				opts.padding = true
+				opts.sticky = true
+				opts.ignore = nil
 			end
 		end,
 	},
